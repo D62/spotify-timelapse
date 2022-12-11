1ab65d84f8e93b2ad02d4dad6b37e10f961bae23
David <6476129+D62@users.noreply.github.com>
2022-12-11 18:04:23 +0100

    Corrected image URLs

--- app.py
+++ app.py
@@ -26,19 +26,19 @@ if __name__ == "__main__":
         st.write("""In order to download your data from Spotify, you need to process as follow:""")
         st.write("""1) Open your Spotify privacy page: https://www.spotify.com/account/privacy/""")
         st.write("""2) Scroll down to the "Download your data" section""")
-        st.image("images\step1.png")
+        st.image("https://raw.githubusercontent.com/D62/spotify-timelapse/main/images/step1.png")
         st.write("""3) Tick the box in the "Extended streaming history" panel""")
         st.write("""4) Press the "Request data" button""")
         st.write("""5) You will receive a link to confirm the request in your email, click on "Confirm" """)
-        st.image("images\step2.png")
+        st.image("https://raw.githubusercontent.com/D62/spotify-timelapse/main/images/step1.png")
         st.write("""6) Wait until you receive your data (this can take up to 30 days)""")
         st.write("""7) Once your data is ready to download, you will get an email with a link to download the *my_spotify_data.zip* file""")
-        st.image("images\step3.png")
+        st.image("https://raw.githubusercontent.com/D62/spotify-timelapse/main/images/step1.png")
         st.write("""You are now ready to upload *my_spotify_data.zip* in the form below.""")
 
     # input form
     with st.form(key="Form"):
-        uploaded_file = st.file_uploader("Upload your *my_spotify_data.zip* file", type="zip")
+        uploaded_file = st.file_uploader("Upload your *my_spotify_data.zip* file (see above for instructions to request it)", type="zip")
         today = datetime.date.today()
         last_week = today - datetime.timedelta(days=7)
         start_date, end_date = st.date_input(
