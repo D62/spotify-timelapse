c2994d3ebecbfe55636ec8ad5d7e675f12dec0da
David <6476129+D62@users.noreply.github.com>
2022-12-12 10:11:56 +0100

    Images updated

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ if __name__ == "__main__":
         st.write("""3) Tick the box in the "Extended streaming history" panel""")
         st.write("""4) Press the "Request data" button""")
         st.write("""5) You will receive a link to confirm the request in your email, click on "Confirm" """)
-        st.image("https://raw.githubusercontent.com/D62/spotify-timelapse/main/images/step1.png")
+        st.image("https://raw.githubusercontent.com/D62/spotify-timelapse/main/images/step2.png")
         st.write("""6) Wait until you receive your data (this can take up to 30 days)""")
         st.write("""7) Once your data is ready to download, you will get an email with a link to download *my_spotify_data.zip*""")
-        st.image("https://raw.githubusercontent.com/D62/spotify-timelapse/main/images/step1.png")
+        st.image("https://raw.githubusercontent.com/D62/spotify-timelapse/main/images/step3.png")
         st.write("""You are now ready to upload *my_spotify_data.zip* in the form below.""")
 
     # set dummy min/max dates before file is uploaded
